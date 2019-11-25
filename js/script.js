543d3432197affdafee908dc320a8d83390c5885
Aleksandr Jevlentjev <jevlentjev@gmail.com>
2019-11-25 18:04:23 +0200

    particles fixed

--- js/script.js
+++ js/script.js
@@ -10,11 +10,16 @@ setTimeout(function () {
 document.getElementById("about-toggle").checked = false;
 
 
+//--------------------------------------------------------------
 //Particle background
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+
+const body = document.getElementById("body");
+
+const rect = body.getBoundingClientRect();
+canvas.width = rect.width;
+canvas.height = rect.height;
 
 let particlesArray;
 
@@ -22,7 +27,7 @@ let particlesArray;
 let mouse = {
     x: null,
     y: null,
-    radius: (canvas.height / 80) * (canvas.width / 80)
+    radius: (canvas.height / 130) * (canvas.width / 130)
 }
 
 window.addEventListener('mousemove',
@@ -30,7 +35,7 @@ window.addEventListener('mousemove',
         mouse.x = event.x;
         mouse.y = event.y;
     }
-)
+);
 
 //create particle
 class Particle {
@@ -70,18 +75,22 @@ class Particle {
         if (distance < mouse.radius + this.size) {
             if (mouse.x < this.x && this.x < canvas.width - this.size * 10) {
                 this.x += 10;
+                this.directionX = -this.directionX;
             }
 
             if (mouse.x > this.x && this.x > this.size * 10) {
                 this.x -= 10;
+                this.directionX = -this.directionX;
             }
 
             if (mouse.y < this.y && this.y < canvas.height - this.size * 10) {
                 this.y += 10;
+                this.directionY = -this.directionY;
             }
 
             if (mouse.y > this.y && this.y > this.size * 10) {
                 this.y -= 10;
+                this.directionY = -this.directionY;
             }
         }
 
@@ -96,10 +105,10 @@ class Particle {
 //create particle array
 const init = () => {
     particlesArray = [];
-    let numberOfParticles = 50; //(canvas.height * canvas.width) / 9000
+    let numberOfParticles = (canvas.height * canvas.width) / 9000;
 
     for (let i = 0; i < numberOfParticles; i++) {
-        let size = (Math.random() * 5) + 1;
+        let size = (Math.random() * 1) + 1;
         let x = (Math.random() * ((innerWidth - size * 2) - (size * 2)) + size * 2);
         let y = (Math.random() * ((innerHeight - size * 2) - (size * 2)) + size * 2);
         let directionX = (Math.random() * 5) - 2.5;
@@ -110,14 +119,78 @@ const init = () => {
     }
 }
 
+const connect = () => {
+    for (let a = 0; a < particlesArray.length; a++) {
+        for (let b = a; b < particlesArray.length; b++) {
+            let distance = ((particlesArray[a].x - particlesArray[b].x) * (particlesArray[a].x - particlesArray[b].x)) +
+                ((particlesArray[a].y - particlesArray[b].y) * (particlesArray[a].y - particlesArray[b].y));
+
+            if (distance < (canvas.width / 9) * (canvas.height / 9)) {
+                ctx.strokeStyle = "#121212";
+                ctx.lineWidth = 1;
+                ctx.beginPath();
+                ctx.moveTo(particlesArray[a].x, particlesArray[a].y);
+                ctx.lineTo(particlesArray[b].x, particlesArray[b].y);
+                ctx.stroke();
+            }
+        }
+    }
+}
+
+//animation loop
 const animate = () => {
     requestAnimationFrame(animate);
-    ctx.clearRect(0, 0, innerWidth, innerHeight);
+    ctx.clearRect(0, 0, innerWidth, innerHeight * 2);
 
     for (let i = 0; i < particlesArray.length; i++) {
         particlesArray[i].update();
     }
+
+    connect();
 }
+
+//resize canvas
+const resizeInstant = () => {
+    const rect = body.getBoundingClientRect();
+    canvas.width = rect.width;
+    canvas.height = rect.height;
+    // init();
+}
+
+//resize canvas 25 times in .35s gap
+const resizeDelay = () => {
+    var callCount = 0;
+    var repeater = setInterval(function () {
+
+        if (callCount < 25) {
+
+            const rect = body.getBoundingClientRect();
+
+            if (document.getElementById('projects-toggle').checked == true) {
+                //is opening
+                canvas.height = rect.height;
+            } else {
+                //is closing
+                canvas.height = rect.height - 58;
+            }
+
+            callCount += 1;
+
+        } else {
+            clearInterval(repeater);
+
+            const rect = body.getBoundingClientRect();
+            canvas.height = rect.height;
+        }
+
+    }, 14);
+}
+
+window.addEventListener('resize', resizeInstant);
+document.getElementById('projects-toggle').addEventListener('input', resizeDelay);
+
+
+
 
 init();
 animate();
